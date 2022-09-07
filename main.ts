54370c5e265d8cc127effea2dbefbfa6c7ccec19
A01254893 <112582801+A01254893@users.noreply.github.com>
2022-09-07 12:44:48 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
-let Texto = 0
-let N1 = 0
-let N2 = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(Num)
+    Num += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    Texto = "chau"
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(Num)
+    Num += -1
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showString(Texto)
+})
+let Num = 0
+let Texto = ""
+Texto = "Hola"
+Num = 15
